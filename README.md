6bb00a895825b3e6d9215297729d1336cdce1491
Victor Ivan <victor.simoes@brmalls.com.br>
2022-08-10 08:25:46 -0300

    chore: adding prisma and updating readme

--- README.md
+++ README.md
@@ -3,4 +3,36 @@
 This service is responsible for authentication and authorization from KDDW Users.
 
 - The authentication is made by github using OAuth
-- The authorization is made by the own KDDW service
+- The authorization is made by the own Auth service
+
+### Installation and Setup
+
+- First clone this repo
+
+```bash
+git clone git@github.com:vitoivan/kddw-auth-service.git && cd kddw-auth-service
+```
+
+- Install dependencies (**with yarn plz**)
+
+```bash
+yarn install
+```
+
+- Run the local database with `docker-compose`
+
+```bash
+docker compose up
+```
+
+- Run the migrations
+
+```bash
+yarn run migrations
+```
+
+- Start the project
+
+```bash
+yarn run start:dev
+```
---
+++ prisma/migrations/20220810111340_first_migration/migration.sql
@@ -0,0 +1,56 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Role" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Role_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "UserJoinRole" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "roleId" INTEGER NOT NULL,
+    "assignedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "assignedBy" TEXT NOT NULL,
+
+    CONSTRAINT "UserJoinRole_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_RoleToUser" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Role_name_key" ON "Role"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_RoleToUser_AB_unique" ON "_RoleToUser"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_RoleToUser_B_index" ON "_RoleToUser"("B");
+
+-- AddForeignKey
+ALTER TABLE "UserJoinRole" ADD CONSTRAINT "UserJoinRole_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "UserJoinRole" ADD CONSTRAINT "UserJoinRole_roleId_fkey" FOREIGN KEY ("roleId") REFERENCES "Role"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_RoleToUser" ADD CONSTRAINT "_RoleToUser_A_fkey" FOREIGN KEY ("A") REFERENCES "Role"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_RoleToUser" ADD CONSTRAINT "_RoleToUser_B_fkey" FOREIGN KEY ("B") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
